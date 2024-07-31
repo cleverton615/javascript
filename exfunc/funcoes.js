df10de2f2b0c9cedfd0c99fb66ce398394131f06
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-07-30 23:34:47 -0300

    exercicios de funções

---
+++ exfunc/funcoes.js
@@ -0,0 +1,37 @@
+/*Funcação VAZIA(VOID)
+let valor = 20
+function incrementa() {
+    valor = 20 + 30
+}
+
+incrementa()
+console.log(valor)
+*/
+
+
+/*Função com Parâmetros(s)
+function soma(numero1, numero2) {
+    const somaDosNumeros = numero1 + numero2
+    console.log(somaDosNumeros)
+}
+
+soma(10,4)
+soma(2,5)*/
+
+/*Função Return
+function soma(numero1, numero2) {
+    const somaDosNumeros = numero1 + numero2
+    return somaDosNumeros
+}
+
+const meusNumeros = soma(4,6)
+console.log(meusNumeros)*/
+
+/*Função Arrow
+const multi = (numero1, numero2) => {
+    const multiplicacao = numero1 * numero2
+    return multiplicacao
+}
+
+const resultado = multi(2,5)
+console.log(resultado)*/
