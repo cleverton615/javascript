035ebfda7c21ad623cb9fa205b8b42d4bcbd1361
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2025-03-08 14:02:50 -0300

    revisando condições

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,6 +1,6 @@
-var vel = 59
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel > 60){ // Condição simples!
-    console.log(`Você ultapassou a velocidade permitida, MULTADO!`)
+var vel = 55
+    console.log(`A velocidade do seu carro é ${vel}Km/h`)
+if(vel >= 50) { // Condição simples!
+    console.log('Você ultrapassou o limite de velocidade. MULTADO!')
 }
-console.log(`Dirija sempre usando cinto de segurança!`)
+    console.log('Dirija sempre usando o cinto de segurança!')
