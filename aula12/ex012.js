7c4b92ff8b321796c62ded8f06a168ee0b1684d0
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2025-03-20 19:06:15 -0300

    revisando js

--- aula12/ex012.js
+++ aula12/ex012.js
@@ -1,10 +1,12 @@
 var agora = new Date()
 var hora = agora.getHours()
-console.log(`Agora são extamente ${hora} horas.`)
-if (hora < 12) {
-    console.log(`Bom dia!`)
-} else if (hora <= 18) {
-    console.log(`Bota tarde!`)
-} else {
-    console.log(`Boa noite!`)
+console.log(`Agora são ${hora} horas.`)
+if (hora >= 18 && hora <= 24) {
+    console.log('Agora é noite!')
+}else if (hora >= 0 && hora < 12){
+    console.log('Agora é manhã!')
+} else if (hora > 24) {
+    console.log('Hórario inexistente!')
+} else if (hora >= 12 && hora <= 17){
+    console.log('Agora é tarde!')
 }
